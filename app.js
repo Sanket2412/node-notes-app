3188d27d6f694f1fdf5fca9d97f8fa0ca8b607a6
Sanket Khardekar <khardekarsanket@gmail.com>
2022-03-31 00:51:51 +0530

    Working With Yargs package

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-const chalk= require('chalk');
-const getNotes= require('./notes')
+const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes");
+//Create Add Command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log(chalk.blue.inverse("Adding New Note!"));
+  },
+});
 
-const command= process.argv[2];
-if (command === 'add') {
-    console.log(chalk.blue('Adding Note'));
-}
-else
-{
-    console.log(chalk.red('No Command Found!'));
-}
+//Create Remove Command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log(chalk.blue.inverse("Removing Note!"));
+  },
+});
 
+//Create List Command
+yargs.command({
+  command: "list",
+  describe: "List All Notes",
+  handler: function () {
+    console.log(chalk.blue.inverse("All List Displayed!"));
+  },
+});
+
+//Create Read Command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log(chalk.blue.inverse("Reading Note!"));
+  },
+});
+yargs.parse();
 // const fs=require('fs');
 // const name=require('./utils');
 // //fs.writeFileSync('notes.txt','This File was created by Node js!');

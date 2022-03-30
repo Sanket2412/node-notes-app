c7e7a9e6df5ff5bf86ca41a2d464fa7e6fe5b69b
Sanket Khardekar <khardekarsanket@gmail.com>
2022-03-30 23:45:49 +0530

    Inculded new package Chalk
    install and used new module chalk

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const validator=require('validator')
+const chalk= require('chalk');
 const getNotes= require('./notes');
 
 console.log(getNotes());
-console.log(validator.isURL('https://google.com'));
+console.log(chalk.green('Success!!!!'));
+
 
 
 
@@ -11,3 +12,6 @@ console.log(validator.isURL('https://google.com'));
 // //fs.writeFileSync('notes.txt','This File was created by Node js!');
 // console.log(name);
 //fs.appendFileSync('notes.txt','This is appended Content :)');
+
+//const validator=require('validator')
+//console.log(validator.isURL('https://google.com'));

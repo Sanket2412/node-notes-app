02fe67a15ab07ae1ce4c92fdf96801bf1134fc59
Sanket Khardekar <khardekarsanket@gmail.com>
2022-03-30 22:58:14 +0530

    Updated Code
    Updated Code created notes.js where all the fs module functions will available and used in app.js using require and exporting modules.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const fs=require('fs');
+const getNotes= require('./notes');
 
-//fs.writeFileSync('notes.txt','This File was created by Node js!');
+console.log(getNotes());
 
-fs.appendFileSync('notes.txt','This is appended Content :)');
+
+
+
+// const fs=require('fs');
+// const name=require('./utils');
+// //fs.writeFileSync('notes.txt','This File was created by Node js!');
+// console.log(name);
+//fs.appendFileSync('notes.txt','This is appended Content :)');
